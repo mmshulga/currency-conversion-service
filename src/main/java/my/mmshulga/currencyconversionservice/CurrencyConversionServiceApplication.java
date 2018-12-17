d093769052237491326577609a0692784be37353
Mikhail Shulga <mmshulga@gmail.com>
2018-12-17 15:45:28 +0300

    Added sleuth.

--- src/main/java/my/mmshulga/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ src/main/java/my/mmshulga/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -1,9 +1,11 @@
 package my.mmshulga.currencyconversionservice;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients("my.mmshulga.currencyconversionservice")
@@ -12,6 +14,11 @@ public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+	}
+
+	@Bean
+	public Sampler alwaysSampler() {
+		return Sampler.ALWAYS_SAMPLE;
 	}
 
 }
